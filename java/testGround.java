2e9c3fb1a51983ff551333a1d562a000cdba1089
Jeremy <jeremyorozco38@gmail.com>
2026-03-26 18:27:16 -0500

    Added more stuff

--- java/testGround.java
+++ java/testGround.java
@@ -1,27 +1,27 @@
 public class testGround {
 
-    public static enum Grade {
-        A, B, C, D, F
-    }
-
     // public static enum Grade {
-    //     A(90),
-    //     B(80),
-    //     C(70),
-    //     D(60),
-    //     F(50)
-    //     ;
-
-    //     private int gradePercent;
-
-    //     private Grade(int g){
-    //         this.gradePercent = g;
-    //     }
-
-    //     public void whatIsYourGrade(){
-    //         System.out.println(this.gradePercent);
-    //     }
+    //     A, B, C, D, F
     // }
+
+    public static enum Grade {
+        A(90),
+        B(80),
+        C(70),
+        D(60),
+        F(50)
+        ;
+
+        private int gradePercent;
+
+        private Grade(int g){
+            this.gradePercent = g;
+        }
+
+        public void whatIsYourGrade(){
+            System.out.println(this.gradePercent);
+        }
+    }
 
 
     public static void main(String[] args) {
--- python/testGround.py
+++ python/testGround.py
@@ -2,14 +2,23 @@ from enum import Enum
 from enum import auto
 
 class Grades(Enum):
+    A = 1
+    B = 2
+    C = 3
+    D = 4
+    F = 5
+
+class Grades(Enum):
     A = "90"
     B = 2
     C = 3
     D = 4
     F = 5
 
-    # def whatIsYourgrade(self):
-    #     print(f'', self.value)
+    
+
+    def whatIsYourgrade(self):
+        print(f'', self.value)
 
 class MainMethodTest():
     def mainMethod():
