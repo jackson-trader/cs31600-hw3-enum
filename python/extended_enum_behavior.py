4a823ac386afd834f071109c11e1fa071a864098
alizardi <aarondlizardi@gmail.com>
2026-03-28 01:47:34 -0500

    Update extended enum behavior implementation

--- python/extended_enum_behavior.py
+++ python/extended_enum_behavior.py
@@ -14,29 +14,13 @@ for i in Light:
     print(i.value[1]) # access the second element of the tuple
     print(i.name)
     
-class Light2 (Enum):
-    RED = ("STOP", 1)
-    YELLOW = ("YIELD", 2)
-    GREEN = ("GO", 3)
-    BLUE = (None) # since python enforces initialization in enum, 
-    
-print("\nBeyond normal iteration for Light2\n")
-x = None
+print("\nBeyond normal iteration\n")
 
-for i in Light2:
-    print(i)
-    print(i.value) 
-    print(i.value[0]) 
-    print(i.value[1]) 
-    print(i.name)
-  
+x = None
+print(x.value) # this will raise an AttributeError since x is None and does not have a value attribute
   
 '''
-the for loop for Light2 produced a TypeError when it tried to access the elements of the tuple for the BLUE member, 
-which has a value of None.
-
-python doesn't explicitly allow 'null'or 'uninitialized' values in enums.
-
-What if we stored the values in an __init__ method instead of as a tuple?
+since x is a standalone variable that is not an instance of the Light enum, it does not 
+have the attributes and methods associated with the Light enum, thus giving an AttributeError.
 
 '''
